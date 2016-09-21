23b7ab2b2ee79b53b3ddeb130ca1c2b909661752
Rolly Maulana Awangga <rolly@awang.ga>
2016-09-21 14:04:41 +0700

    add response

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
+import gitar
 from cgi import escape
+import config
 
 def application(environ, start_response):
 	uri = environ['REQUEST_URI']
 
 	uri = escape(uri)
-	response_body = "isina : "+uri
+	gr = gitar.Gitar()
+
+	if uri == config.uri1:
+		respon=gr.gitpull(config.host1,config.username1,config.password2)		
+	elif uri == config.uri2:
+		respon=gr.gitpull(config.host2,config.username1,config.password2)
+	else:
+		respon="oke"
+
 
 	start_response('200 OK', [('Content-Type', 'text/html')])
 	
-	return [response_body]
+	return [respon]
 
