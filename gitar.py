040815eb6f6bfe8bf97c764931549c2d7358a7e5
Rolly Maulana Awangga <rolly@awang.ga>
2016-09-21 13:38:49 +0700

    add gitpull

--- gitar.py
+++ gitar.py
@@ -7,7 +7,14 @@ created by Rolly Maulana Awangga
 
 import paramiko
 
+
 class Gitar(object):
-	def __init__(self, recipient):
-		self.
+	def __init__(self):
+		self.ssh=paramiko.SSHClient()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+	def gitpull(self,dir,host,user,pwd) 
+		ssh.connect(host,username=user,password=pwd)
+		stdin,stdout,stderr=ssh.exec_command("cd "+dir+";git pull")
+		return stdout.readlines()
 
